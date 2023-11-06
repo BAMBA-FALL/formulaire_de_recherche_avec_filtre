c901c91bfffe84c24b168ca46c70ba4e0d325f24
BAMBA-FALL <khadimmbackefall1@gmail.com>
2023-11-06 23:32:10 +0100

    liste des tâches moderne

--- index.js
+++ index.js
@@ -1,80 +1,117 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const formulaireRecherche = document.getElementById("formulaire-recherche");
-    const motCleInput = document.getElementById("mot-cle");
-    const listeResultats = document.getElementById("resultats");
+const projet = {
+    nom: "Application de gestion de projet",
+    taches: [
+        {
+            nom: "s'occuper de la partie front de l'application",
+            priorite: "haute"
+        },
+        {
+            nom: "s'occuper de la partie back de l'application",
+            priorite: "moyenne"
+        },
+        {
+            nom: "s'occuper de la mise à jour de l'application",
+            priorite: "faible"
+        },
+    ],
 
-    formulaireRecherche.addEventListener("submit", function(e) {
-        e.preventDefault();
+    ajoutertache: function(nom, priorite) {
+        this.taches.push({ nom: nom, priorite: priorite });
+    },
 
-        const motCle = motCleInput.value;
+    supprimertache: function(index) {
+        this.taches.splice(index, 1);
+    },
+};
 
-        // Exemple : Effectuer une recherche basée sur le mot-clé
-        const fauxResultats = [
-            "iPhone 13 Pro Max",
-            "iPhone 13 Pro",
-            "iPhone 13",
-            "iPhone 12 Pro Max",
-            "iPhone 12 Pro",
-            "iPhone 12",
-            "iPhone SE (2e génération)",
-            "iPhone 11 Pro Max",
-            "iPhone 11 Pro",
-            "iPhone 11",
-            "iPhone X",
-            "iPhone 8 Plus",
-            "iPhone 8",
-            "iPhone 7 Plus",
-            "iPhone 7",
-            "iPhone 6s Plus",
-            "iPhone 6s",
-            "iPhone 6 Plus",
-            "iPhone 6",
-            "iPhone 5s",
-        ];
+// ====================================================== //
+//  CREATION DE LA FONCTION QUI GENERE LA LISTE DES TACHES LES LI ET LES BOUTON SUPPRIMER ET MODIFIER  //
+// ====================================================== //
 
-        const resultatsFiltres = filtrerResultats(fauxResultats, motCle);
+//fonction pour les li 
+// Fonction pour créer les éléments de tâche sous forme de cartes
+function creerElementTache(tache, index) {
+    // Créez un élément de carte (div)
+    const carte = document.createElement("div");
+    carte.classList.add("carte"); // Ajoutez une classe CSS pour les cartes
 
-        afficherResultats(resultatsFiltres, listeResultats);
-    });
+    // Créez des éléments de contenu pour la carte
+    const nomTache = document.createElement("p");
+    nomTache.textContent = `Nom : ${tache.nom}`;
 
-    listeResultats.addEventListener("mouseover", function(e) {
-        if (e.target && e.target.nodeName == "LI") {
-            e.target.classList.add("resultat-survol");
+    const prioriteTache = document.createElement("p");
+    prioriteTache.textContent = `Priorité : ${tache.priorite}`;
+
+    // Créez un bouton de suppression
+    const boutonSupprimer = document.createElement("button");
+    boutonSupprimer.innerHTML = '<i class="fas fa-trash-alt"></i> Supprimer';
+    boutonSupprimer.classList.add("bouton-supprimer");
+
+    boutonSupprimer.addEventListener("click", () => {
+        const confirmation = confirm("Êtes-vous sûr de vouloir supprimer cette tâche ?");
+        if (confirmation) {
+            projet.supprimertache(index);
+            afficherTache();
         }
     });
 
-    listeResultats.addEventListener("mouseout", function(e) {
-        if (e.target && e.target.nodeName == "LI") {
-            e.target.classList.remove("resultat-survol");
+    // Créez un bouton de modification
+    const boutonModifier = document.createElement("button");
+    boutonModifier.innerHTML = '<i class="fas fa-edit"></i> Modifier';
+    boutonModifier.classList.add("bouton-modifier");
+
+    boutonModifier.addEventListener("click", () => {
+        const nouveauNom = prompt("Entrez le nouveau nom de la tâche", tache.nom);
+        const nouvellePriorite = prompt("Entrez la nouvelle priorité de la tâche", tache.priorite);
+        if (nouveauNom !== null && nouvellePriorite !== null) {
+            tache.nom = nouveauNom;
+            tache.priorite = nouvellePriorite;
+            afficherTache();
         }
     });
 
-    // Ajouter un gestionnaire de clic pour les éléments de résultat
-    listeResultats.addEventListener("click", function(e) {
-        if (e.target && e.target.nodeName == "LI") {
-            // Récupérer le texte de l'élément de résultat
-            const texteResultat = e.target.textContent;
-            // Rediriger vers la page "resultat.html" avec le texte en tant que paramètre
-            window.location.href = `resultat.html?result=${encodeURIComponent(texteResultat)}`;
-        }
+    // Ajoutez les éléments au contenu de la carte
+    carte.appendChild(nomTache);
+    carte.appendChild(prioriteTache);
+    carte.appendChild(boutonSupprimer);
+    carte.appendChild(boutonModifier);
+
+    return carte;
+}
+
+
+//ici on récupère l'élément UL, où nous allons afficher les tâches
+const listeTaches = document.getElementById("liste-taches");
+
+//Ici, on parcourt les tâches du projet et ajoutez-les à la liste UL avec les boutons "Supprimer" et "Modifier"
+function afficherTache() {
+    //ici on récupère la liste des tâches créées
+    listeTaches.innerHTML = "";
+    projet.taches.forEach((tache, index) => {
+        const elementTache = creerElementTache(tache, index);
+        listeTaches.appendChild(elementTache);
     });
+}
 
-    function filtrerResultats(resultats, motCle) {
-        // Filtrer les résultats en fonction du mot-clé
-        return resultats.filter(function (resultat) {
-            return resultat.toLowerCase().includes(motCle.toLowerCase());
-        });
-    }
+//ici on récupère les éléments du formulaire
+const formulaireTache = document.getElementById("formulaire-tache");
+const nomTacheInput = document.getElementById("nom-tache");
+const prioriteTacheInput = document.getElementById("priorite-tache");
 
-    function afficherResultats(resultats, listeResultats) {
-        // Réinitialiser la liste des résultats
-        listeResultats.innerHTML = "";
+//ici on gère l'ajout des tâches lorsqu'il y a un clic (donc on attend un événement) 
+const ajouterTacheButton = document.getElementById("ajouter-tache");
+ajouterTacheButton.addEventListener("click", () => {
+    const nomTache = nomTacheInput.value;
+    const prioriteTache = prioriteTacheInput.value;
 
-        // Afficher les résultats
-        resultats.forEach(function (resultat) {
-            const li = document.createElement("li");
-            li.textContent = resultat;
-            listeResultats.appendChild(li);
-        });
+    if (nomTache && prioriteTache) {
+        projet.ajoutertache(nomTache, prioriteTache);
+        afficherTache(); // Met à jour la liste des tâches après l'ajout
+        nomTacheInput.value = ''; // Réinitialise les champs du formulaire
+        prioriteTacheInput.value = '';
+    }else{
+        alert("veuillez saisir le nom et la priorité de votre tâche")
     }
 });
+
+afficherTache();
